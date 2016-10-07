611b02e95d92fcf9ba4fe78367381d66b4856e29
Johan Ospina <Johan Ospina>
2016-10-07 17:29:56 -0400

    First Commit

---
+++ Murphy/Assets/BostonVR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BostonVR : MonoBehaviour {
+
+    public int someNum;
+
+    public Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hey this is me");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
