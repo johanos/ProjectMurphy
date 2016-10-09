f2674f5b2f04329e0d452436c13fef4aebe705b1
Johan Ospina <johanos@bu.edu>
2016-10-09 19:01:05 -0400

    Merging with Andrews Dupuis' input

---
+++ Murphy/Assets/faceMe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class faceMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(Camera.main.transform);
+        this.transform.up = Vector3.up;
+	}
+}
