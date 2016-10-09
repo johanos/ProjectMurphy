169220047ffe4ddf2b97127aa840f9588d331b5c
Johan Ospina <Johan Ospina>
2016-10-09 01:57:12 -0400

    Hope will carry us through

---
+++ Murphy/Assets/Scripts/MicrophoneController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneController : MonoBehaviour
+{
+
+    private MicrophoneManager microphoneManager;
+    private bool recordingOnGoing; 
+	// Use this for initialization
+	void Start ()
+	{
+	    Debug.Log("Hello");
+	    recordingOnGoing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnSelect()
+    {
+        
+        microphoneManager = gameObject.GetComponent<MicrophoneManager>();
+        Debug.Log("Selected");
+        if (!recordingOnGoing)
+        {
+            microphoneManager.StartRecording();
+            recordingOnGoing = true;
+
+        }
+        else
+        {
+            microphoneManager.StopRecording();
+            recordingOnGoing = false;
+        }
+
+    }
+}
